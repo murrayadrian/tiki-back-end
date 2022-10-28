d6673a600ceb9c5bd5acf40379a0215019e6c8ba
bacnguyen <1005@gmail.com>
2022-10-28 18:53:14 +0700

    create new project

---
+++ TikiApi/src/main/java/com/tikifake/TikiApiApplication.java
@@ -0,0 +1,13 @@
+package com.tikifake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikiApiApplication.class, args);
+	}
+
+}
---
+++ TikiApi/src/test/java/com/tikifake/TikiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikifake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
